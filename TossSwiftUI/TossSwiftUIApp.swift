162ebf57ad1873cd98d0bee8ac927115976498b6
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-04-16 12:52:07 +0900

    Initial Commit

---
+++ TossSwiftUI/TossSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TossSwiftUIApp.swift
+//  TossSwiftUI
+//
+//  Created by 조유진 on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TossSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
